7b72ed734974054ddb43ccfb9e60dc95aacfcf28
Lenkura <62018399+Lenkura@users.noreply.github.com>
2020-11-01 20:29:39 +1100

    Light Sensor code

---
+++ lightsensor.py
@@ -0,0 +1,24 @@
+import board
+import time
+import busio
+import adafruit_tsl2591
+i2c = busio.I2C(board.SCL, board.SDA)
+sensor = adafruit_tsl2591.TSL2591(i2c)
+
+#print('Light: {0}lux'.format(sensor.lux))
+#print('Visible: {0}'.format(sensor.visible))
+#print('Infrared: {0}'.format(sensor.infrared))
+sensor.gain = adafruit_tsl2591.GAIN_LOW
+
+while True:
+    visible = sensor.visible
+    lux = sensor.lux
+    print('Visible',visible)
+    print('All',lux)
+    time.sleep(3)
+
+# Typical ranges:
+# <10 Dark
+# 20-40 Shade/low lighting
+# 40-60 Lit room
+
