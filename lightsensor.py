be9d539204ef81fa7ed2e9a55ce762c0da9123ce
Raymond <62018399+Lenkura@users.noreply.github.com>
2020-11-05 16:25:05 +1100

    Update lightsensor.py
    Change sensor type TSL2561

--- lightsensor.py
+++ lightsensor.py
@@ -1,24 +1,26 @@
 import board
-import time
 import busio
-import adafruit_tsl2591
+import adafruit_tsl2561
 i2c = busio.I2C(board.SCL, board.SDA)
-sensor = adafruit_tsl2591.TSL2591(i2c)
+sensor = adafruit_tsl2561.TSL2561(i2c)
 
-#print('Light: {0}lux'.format(sensor.lux))
-#print('Visible: {0}'.format(sensor.visible))
-#print('Infrared: {0}'.format(sensor.infrared))
-sensor.gain = adafruit_tsl2591.GAIN_MED
+# sensor.gain = 1
 
 while True:
-    visible = sensor.visible
-    lux = sensor.lux
-    print('Visible',visible)
-    print('All',lux)
+    print('Lux: {}'.format(sensor.lux))
+    print('Broadband: {}'.format(sensor.broadband))
+    print('Infrared: {}'.format(sensor.infrared))
+    print('Luminosity: {}'.format(sensor.luminosity))
+#     visible = sensor.visible
+#     lux = sensor.lux
+#     print('Visible',visible)
+#     print('All',lux)
     time.sleep(3)
 
 # Typical ranges:
 # <10 Dark
 # 20-40 Shade/low lighting
 # 40-60 Lit room
+
+
 
