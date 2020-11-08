1ccbd8439a7228a0a2bb377bb46f70b75e22ff42
Lenkura <62018399+Lenkura@users.noreply.github.com>
2020-11-08 18:57:54 +1100

    Update TransmitterTrigger.py
    Added keyboard inputs

--- TransmitterTrigger.py
+++ TransmitterTrigger.py
@@ -1,2 +1,10 @@
 import os
+import keyboard
+while True:
+     try: 
+        if keyboard.is_pressed('t'):
+            print('You Pressed A Key!')
+            break  # finishing the loop
+    except:
+        break  # if user pressed a key other than the given key the loop will break
 os.system('python3 Transmitter.py -p 1615 -t 4 12345')
