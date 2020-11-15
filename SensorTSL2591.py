b6d3269a5c6191b9e71435072eba78fbeefd6251
Raymond <62018399+Lenkura@users.noreply.github.com>
2020-11-15 23:28:33 +1100

    Update SensorTSL2591.py
    Cut to only lux, rounded the end figure

--- SensorTSL2591.py
+++ SensorTSL2591.py
@@ -9,29 +9,21 @@ sensor = adafruit_tsl2591.TSL2591(i2c)
 sensor.gain = adafruit_tsl2591.GAIN_MED
 #Integration time, time the sensor will gather data per reading (1,2,3,4,5,600)
 adafruit_tsl2591.INTEGRATIONTIME_300MS
-
-print('Light: {0}lux'.format(sensor.lux))
-print('Visible: {0}'.format(sensor.visible))
-print('Infrared: {0}'.format(sensor.infrared))
-
+    
 #Categories
 #Visible - digital reading value for visible spectrum
 #Infrared - digital reading value for infrared
 #All - Lux value for entire spectrum
 while True:
-    visible = sensor.visible
-    lux = sensor.lux
-    infrared = sensor.infrared
-    print('Visible',visible)
-    print('Infrared',infrared)
+    lux = round(sensor.lux,2)
     print('All',lux)
     print('*****'*5)
     time.sleep(3)
 
 # Typical ranges:
 # <10 Dark
-# 20-40 Shade/low lighting
-# 40-60 Lit room
-
+# 10-40 Shade/low lighting
+# 40-70 Bright room
+# >70 is a focused light pointed at it.
 
 
